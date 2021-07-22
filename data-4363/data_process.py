842fe90cee13aacb269e2dbbaf10ae18676bcc34
Qunli_hacker <8235636+qunli-hacker@user.noreply.gitee.com>
2021-07-22 03:29:37 +0000

    data process

---
+++ data-4363/data_process.py
@@ -0,0 +1,59 @@
+import csv
+import copy
+from queue import PriorityQueue
+
+for i in range(20):
+    with open("val_"+str(i)+".csv",'w') as f1:
+        f1_csv = csv.writer(f1)
+        f1_csv.writerow(['','vmId','tenantId','vmTypeId','priority','time','flag','during_time'])
+
+
+        with open("val"+str(i)+".csv",'r') as f:
+           f_csv=csv.reader(f)
+           j=0
+           for row in f_csv:
+
+                if j!=0 and row[6]!='':
+
+                    if float(row[5])<float(row[6]):
+                        row[5] = float(row[5])
+                        row[6] = float(row[6])
+                        list=copy.deepcopy(row)
+                        list2 = copy.deepcopy(row)
+                        list.pop(6)
+                        list.append(1)
+                        list.append(float(row[6]) - float(row[5]))
+                        f1_csv.writerow(list)
+                        list2.pop(5)
+                        list2.append(0)
+                        list2.append(float(row[6]) - float(row[5]))
+
+                        f1_csv.writerow(list2)
+                j=j+1
+
+
+for i in range(20):
+    customers = PriorityQueue()
+    with open("val_" + str(i) + ".csv", 'r') as f:
+
+        f_csv = csv.reader(f)
+        j = 0
+        for row in f_csv:
+           # print(row[5])
+            if j != 0:
+
+                prio1=float(row[5])
+                prio2=float(row[6])
+                customers.put(((prio1,prio2),row))
+            j=j+1
+
+    with open("val_"+str(i)+".csv",'w') as f1:
+        f1_csv = csv.writer(f1)
+        f1_csv.writerow(['','vmId','tenantId','vmTypeId','priority','time','flag','during_time'])
+        while not customers.empty():
+            f1_csv.writerow(customers.get()[1])
+
+
+
+
+
